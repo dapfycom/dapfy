37c23aac30424080a097dd01a8a8b66bdf156751
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-09-16 02:55:53 -0400

    fix time

--- src/utils/functions/dates.ts
+++ src/utils/functions/dates.ts
@@ -2,11 +2,26 @@ import moment from "moment";
 
 // convert seconds into a readable hour:minute:second format
 
-export const secondsToHms = (seconds: number) => {
-  if (!seconds) return "00:00:00";
+export const secondsToHms = (seconds?: number) => {
+  if (!seconds) return "0s";
   const duration = moment.duration(seconds, "seconds");
   const hours = duration.hours();
   const minutes = duration.minutes();
   const secs = duration.seconds();
-  return `${hours}:${minutes}:${secs}`;
+
+  let timeString = "";
+
+  if (hours > 0) {
+    timeString += `${hours}h `;
+  }
+
+  if (minutes > 0) {
+    timeString += `${minutes}m `;
+  }
+
+  if (secs > 0) {
+    timeString += `${secs}s`;
+  }
+
+  return timeString.trim();
 };
