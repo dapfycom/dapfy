3211c9cfa0e9dcb52ffa07ef8805093cd75ff499
Mandy9943 <armandoc9943@gmail.com>
2023-09-26 07:26:33 -0400

    add xpayment card

--- src/views/DashboardView/Dashboard.tsx
+++ src/views/DashboardView/Dashboard.tsx
@@ -4,6 +4,7 @@ import DashboardHeading from "./common/DashboadHeading";
 import { ContentTable } from "./common/HoldingsTable";
 import RewardsCard from "./common/RewardsCard";
 import Settings from "./common/Settings";
+import XPaymentCard from "./common/XPaymentCard";
 
 const Dashboard = () => {
   return (
@@ -12,8 +13,9 @@ const Dashboard = () => {
       <RewardsCard />
       {/* <Cards /> */}
       <Settings />
-      <div className="grid  gap-8 grid-cols-1">
+      <div className="grid  gap-8 grid-cols-1 md:grid-cols-2">
         <ContentTable />
+        <XPaymentCard />
         {/* <ShowcaseNFTs /> */}
       </div>
     </Container>
---
+++ src/views/DashboardView/common/XPaymentCard.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+const XPaymentCard = () => {
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle>X Payments</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default XPaymentCard;
