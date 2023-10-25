a44377d2dc5cd54ef0a76a1fc322da1a88a7d4b7
Mandy9943 <armandoc9943@gmail.com>
2023-10-25 10:34:31 -0400

    [ADD] - on Dashboard -> Rewards Centre -> add text with steps the user need to take in order to get rewarded

--- src/views/DashboardView/common/RewardsCard.tsx
+++ src/views/DashboardView/common/RewardsCard.tsx
@@ -9,6 +9,7 @@ import {
 import ChartCard from "./ChartCard";
 
 const RewardsCard = () => {
+  const isConnectedWithXAccount = false;
   return (
     <Card>
       <CardHeader>
@@ -16,17 +17,30 @@ const RewardsCard = () => {
         <CardDescription>Claim, Track, and Enjoy Your Benefits</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-4">
-          <Reward reward="Reward Power" value={"3.5 %"} />
-          <Reward reward="Overall Rewards" value={"1.2 EGLD"} />
-          <Reward reward="Uncollected rewards" value={"4.2 EGLD"} />
-        </div>
-        <div className="w-full flex mt-4">
-          <Button className="min-w-[150px]">Claim</Button>
-        </div>
-        <div className="mt-8">
-          <ChartCard />
-        </div>
+        {isConnectedWithXAccount ? (
+          <>
+            <div className="flex flex-col gap-4">
+              <Reward reward="Reward Power" value={"3.5 %"} />
+              <Reward reward="Overall Rewards" value={"1.2 EGLD"} />
+              <Reward reward="Uncollected rewards" value={"4.2 EGLD"} />
+            </div>
+            <div className="w-full flex mt-4">
+              <Button className="min-w-[150px]">Claim</Button>
+            </div>
+            <div className="mt-8">
+              <ChartCard />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="mb-2">Follow these steps to get started:</div>
+            <ol className="list-decimal list-inside">
+              <li>Connect your X account</li>
+              <li>Interact or write about @dapfycom</li>
+              <li>Earn rewards.</li>
+            </ol>
+          </>
+        )}
       </CardContent>
     </Card>
   );
