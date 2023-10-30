2f18af5f7b5fc67ecc2080d6650a74f08784cde2
Mandy9943 <armandoc9943@gmail.com>
2023-10-30 02:49:52 -0400

    fix Upgrade page

--- src/app/[lang]/upgrade/page.tsx
+++ src/app/[lang]/upgrade/page.tsx
@@ -1,14 +1,8 @@
-import prisma from "@/lib/db";
-import { getPrivateKey } from "@/lib/server-mx-transactions";
 import UpgradeView from "@/views/UpgradeView/Upgrade";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Upgrade",
 };
 export default async function Upgrade() {
-  getPrivateKey();
-  const v = await prisma.wallet.findMany();
-  console.log(v);
-
   return <UpgradeView />;
 }
