988c293040da08e7a1e1f7b97f49c21f1b8e6761
Mandy9943 <armandoc9943@gmail.com>
2023-11-05 16:48:51 -0500

    update email from

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -5,7 +5,7 @@ import KoalaWelcomeEmail from "../../react-email/emails/welcome";
 const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendWelcomeEmail = async (email: string) => {
   await resend.emails.send({
-    from: "Dapfy <onboarding@pinocli.com>",
+    from: `Dapfy <${process.env.EMAIL_HOST}>`,
     to: [email],
     subject: "Welcome to Dapfy 🎉",
     react: KoalaWelcomeEmail({ externnalLinks: externnalLinks }),
