7db7b55f5d1f24a582ada1a29c557136ec4be11e
Mandy9943 <armandoc9943@gmail.com>
2023-11-05 16:34:29 -0500

    update REWARDS_BASE_URL

--- src/services/rest/rewards/index.ts
+++ src/services/rest/rewards/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const REWARDS_BASE_URL = process.env.NEXT_REWARDS_URL;
+export const REWARDS_BASE_URL = process.env.NEXT_PUBLIC_REWARDS_URL;
 
 const axiosRewards = axios.create({
   baseURL: REWARDS_BASE_URL,
