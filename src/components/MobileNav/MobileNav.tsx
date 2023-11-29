7c620b5adff1a8b766e17eda3527a4820d7d8eef
Armnando Cesar Martin <armandoc9943@gmail.com>
2023-11-29 07:48:49 -0500

    fix show admin on mobile

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -20,10 +20,11 @@ import { Separator } from "../ui/separator";
 const MobileNav = () => {
   const { isOpen, onClose, onToggle } = useDisclosure();
   const { setTheme, theme } = useTheme();
-  const { handleDisconnect, isLoggedIn } = useAuthentication();
+  const { handleDisconnect, isLoggedIn, isAdmin } = useAuthentication();
 
   const navRoutes = mainSiteRoutes.filter((route) => Boolean(route.path));
   const routes = navRoutes.map((route) => ({
+    ...route,
     href: route.path,
     label: route.title,
   }));
@@ -40,21 +41,25 @@ const MobileNav = () => {
           {/* <SheetTitle className="mb-8">Menu</SheetTitle> */}
 
           <div className="flex flex-col gap-3">
-            {routes.map((route) => (
-              <Fragment key={route.href}>
-                <Button asChild variant={"ghost"} className="justify-start">
-                  <Link
-                    href={route.href || ""}
-                    className={cn("text-sm font-medium transition-colors  ")}
-                    onClick={onClose}
-                  >
-                    {route.label}
-                  </Link>
-                </Button>
+            {routes.map((route) => {
+              if (route.onlyAdmin && !isAdmin) return null;
 
-                <Separator className="my-1" />
-              </Fragment>
-            ))}
+              return (
+                <Fragment key={route.href}>
+                  <Button asChild variant={"ghost"} className="justify-start">
+                    <Link
+                      href={route.href || ""}
+                      className={cn("text-sm font-medium transition-colors  ")}
+                      onClick={onClose}
+                    >
+                      {route.label}
+                    </Link>
+                  </Button>
+
+                  <Separator className="my-1" />
+                </Fragment>
+              );
+            })}
           </div>
         </SheetHeader>
         <SheetFooter className="flex-col">
