b5345ea17ef0976c9abf95c414b11ea31335beac
Armnando Cesar Martin <armandoc9943@gmail.com>
2023-11-30 12:37:32 -0500

    make farms container bigger

--- src/views/FarmView/FarmView.tsx
+++ src/views/FarmView/FarmView.tsx
@@ -1,10 +1,9 @@
 import Container from "@/components/ui-system/Container";
 import FarmComponent from "./commons/FarmComponent/FarmComponent";
 import FarmHeading from "./commons/FarmHeading/FarmHeading";
-import FarmAshSwap from "./commons/FarmAshSwap/FarmAshSwap";
 const FarmView = () => {
   return (
-    <Container className="xl:max-w-[1000px]">
+    <Container className="xl:max-w-[1200px]">
       <div className="flex flex-col items-center text-center mt-5">
         <FarmHeading />
         <div className="flex flex-col gap-3 w-full">
