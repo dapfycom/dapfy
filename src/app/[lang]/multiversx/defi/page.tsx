210ff6678a3a364732e5967b2802c1821957c937
Armnando Cesar Martin <armandoc9943@gmail.com>
2023-12-09 15:37:22 -0500

    remove defi page

--- src/app/[lang]/multiversx/defi/page.tsx
+++
@@ -1,8 +0,0 @@
-import DefiView from "@/views/DefiView";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Defi",
-};
-export default function Defi() {
-  return <DefiView />;
-}
