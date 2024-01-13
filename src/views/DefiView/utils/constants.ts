31106b3517c745930be32b1bcbbfc3cd842f64eb
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-01-13 14:05:12 -0500

    update hatom apy

--- src/views/DefiView/utils/constants.ts
+++ src/views/DefiView/utils/constants.ts
@@ -1,10 +1,10 @@
 export const HatomConfigs = {
   apy: {
-    EGLD: 2.69,
-    USDC: 34.07,
-    USDT: 37.97,
+    EGLD: 6.47,
+    USDC: 24.63,
+    USDT: 27.1,
     BUSD: 0,
-    SEGLD: 1.043,
+    SEGLD: 4.17,
   },
   minDeposit: {
     EGLD: 1,
