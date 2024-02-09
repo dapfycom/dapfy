7215264782efd3d882bc8a5b08ef85b3a94fcce4
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-02-09 09:35:00 -0500

    update

--- src/services/rest/dapfy-api/task.ts
+++ src/services/rest/dapfy-api/task.ts
@@ -1,10 +1,7 @@
 import { IUserToReward } from "@/types/rewards.interface";
-import { format } from "date-fns";
 import axiosDapfy from ".";
 
-export const fetchUserTwitterTask = async (
-  date: string = format(new Date(), "yyyy-LL-dd")
-) => {
+export const fetchUserTwitterTask = async (date?: string) => {
   const { data } = await axiosDapfy.get<{ users: IUserToReward[] }>(
     "task/all",
     {
