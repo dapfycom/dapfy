49d5bd807ae03a623e3e617015bef8b52aead0ca
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-02-12 09:36:24 -0500

    Update title in HeroTagView component

--- src/views/HeroTagView/HeroTagView.tsx
+++ src/views/HeroTagView/HeroTagView.tsx
@@ -11,7 +11,7 @@ const HeroTagView = () => {
     <Container>
       <div className="flex flex-col items-center text-center mt-5">
         <PageHeaderHeading className="mb-6">
-          <span className={"gradienteTitle"}>Make your Herotag</span>
+          <span className={"gradienteTitle"}>Create your Herotag</span>
         </PageHeaderHeading>
         <PageHeaderDescription className="mb-10">
           Associate a herotag to your erd address in multiversx
