8b01fbbb5457a525011cc26eba8b84cc854429d1
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-02-24 13:22:35 -0500

    Add X icon to toast notification

--- src/hooks/useTxNotification.tsx
+++ src/hooks/useTxNotification.tsx
@@ -1,23 +1,32 @@
 import { externnalLinks } from "@/config/routes";
+import { X } from "lucide-react";
 import toast from "react-hot-toast";
 
 const useTxNotification = () => {
   const handleToast = () => {
-    toast.success(
-      <span>
-        💡 Like, comment and retweet{" "}
-        <a
-          href={externnalLinks.twitter}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
+    const toastId = toast.success(
+      <div className="relative">
+        <span>
+          💡 Like, comment and retweet{" "}
+          <a
+            href={externnalLinks.twitter}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500"
+          >
+            @dapfycom
+          </a>{" "}
+          posts on X to be eligible for rewards!
+        </span>
+        <div
+          className="absolute right-0 top-0 cursor-pointer"
+          onClick={() => toast.dismiss(toastId)}
         >
-          @dapfycom
-        </a>{" "}
-        posts on X to be eligible for rewards!
-      </span>,
+          <X fontSize={"14px"} size={"16px"} />
+        </div>
+      </div>,
       {
-        duration: 10000,
+        duration: 6000000,
       }
     );
   };
