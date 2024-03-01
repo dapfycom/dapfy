7e28eee1d807064838704fe8017d73ef60e60cda
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-03-01 11:07:10 -0500

    Refactor CheckLastRewarded component to display rewarded users and provide a link to the transaction

--- src/views/AdminPanelView/Rewards/CheckLastRewarded/CheckLastRewarded.tsx
+++ src/views/AdminPanelView/Rewards/CheckLastRewarded/CheckLastRewarded.tsx
@@ -27,48 +27,64 @@ const CheckLastRewarded = () => {
   if (!data) {
     return <div>Loading...</div>;
   }
-
-  const hexData = decodeBase64ToString(data[0].data || "");
+  const transaction = data[0];
+  const hexData = decodeBase64ToString(transaction.data || "");
   const addressInHex = hexData.split("@")[2];
   const addressList = decodeHexToListOfAddress(addressInHex);
-  console.log({ addressList });
 
   return (
     <div>
       <h3 className="text-xl mb-3 text-green-500">Last users rewarded</h3>
       <div className="mb-2">Users Rewarded : {addressList.length}</div>
-      <div>
-        <Dialog>
-          <DialogTrigger>
-            <Button>Show users</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className="mb-3">List of rewarded users</DialogTitle>
-              <DialogDescription>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 overflow-y-auto max-h-[500px]">
-                  {addressList.map((userAddress) => {
-                    return (
-                      <a
-                        key={userAddress}
-                        href={
-                          selectedNetwork.network.explorerAddress +
-                          "/accounts/" +
-                          userAddress
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500"
-                      >
-                        <div>{formatAddress(userAddress)}</div>
-                      </a>
-                    );
-                  })}
-                </div>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+      <div className="flex flex-wrap gap-2">
+        <div>
+          <Dialog>
+            <DialogTrigger>
+              <Button>Show users</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="mb-3">
+                  List of rewarded users
+                </DialogTitle>
+                <DialogDescription>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 overflow-y-auto max-h-[500px]">
+                    {addressList.map((userAddress) => {
+                      return (
+                        <a
+                          key={userAddress}
+                          href={
+                            selectedNetwork.network.explorerAddress +
+                            "/accounts/" +
+                            userAddress
+                          }
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500"
+                        >
+                          <div>{formatAddress(userAddress)}</div>
+                        </a>
+                      );
+                    })}
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <Button asChild>
+          <a
+            href={
+              selectedNetwork.network.explorerAddress +
+              "/transactions/" +
+              transaction.txHash
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to Transaction
+          </a>
+        </Button>
       </div>
     </div>
   );
