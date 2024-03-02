ca05c900d35fea468d7257c82a2e4f3adeba17b1
Armnando Cesar Martin <armandoc9943@gmail.com>
2024-03-01 21:29:56 -0500

    Revert "Refactor UserAddressHasInteracted function to check for a specific token for burnfy"
    This reverts commit 2400c0b0ac1481677304bedf8a635f7ab5b1794f.

--- src/app/api/use-sc-tool/functions.ts
+++ src/app/api/use-sc-tool/functions.ts
@@ -1,51 +1,8 @@
 import { selectedNetwork } from "@/config/network";
-import { decodeBase64ToString } from "@/lib/coder";
 import { timeStampToSeconds } from "@/lib/date";
 import { fetchTransactions } from "@/services/rest/elrond/transactions";
-import { BytesValue } from "@multiversx/sdk-core/out";
+import { excludeSmartContracts } from "./excluded-sc";
 
-// export const UserAddressHasInteracted = async (payload: {
-//   to: Date;
-//   from: Date;
-//   address?: string;
-// }) => {
-//   if (!payload.address) {
-//     return false;
-//   }
-
-//   const results = await fetchTransactions({
-//     before: timeStampToSeconds(payload.to.getTime()),
-//     after: timeStampToSeconds(payload.from.getTime()),
-//     sender: payload.address,
-//     size: 1000,
-//     status: "success",
-//   });
-
-//   const scAddressArr = Object.entries(selectedNetwork.scAddress);
-
-//   let hasInteracted = false;
-
-//   for (let index = 0; index < scAddressArr.length && !hasInteracted; index++) {
-//     const addressElement = scAddressArr[index];
-
-//     if (excludeSmartContracts.includes(addressElement[0])) {
-//       continue;
-//     }
-
-//     for (let j = 0; j < results.length && !hasInteracted; j++) {
-//       const tx = results[j];
-
-//       if (
-//         tx.receiver === addressElement[1] ||
-//         tx.action?.arguments?.receiver === addressElement[1]
-//       ) {
-//         hasInteracted = true;
-//       }
-//     }
-//   }
-
-//   return hasInteracted;
-// };
 export const UserAddressHasInteracted = async (payload: {
   to: Date;
   from: Date;
@@ -59,25 +16,32 @@ export const UserAddressHasInteracted = async (payload: {
     before: timeStampToSeconds(payload.to.getTime()),
     after: timeStampToSeconds(payload.from.getTime()),
     sender: payload.address,
+    size: 1000,
     status: "success",
-    receiver: "erd1qqqqqqqqqqqqqpgqxetccmv9rjefu2fwtw7a6kkx0tsqtqxpp4ssj8shz4",
-    function: "deposit",
   });
+
+  const scAddressArr = Object.entries(selectedNetwork.scAddress);
 
   let hasInteracted = false;
 
-  results.forEach((transaction) => {
-    const hexData = decodeBase64ToString(transaction.data || "");
+  for (let index = 0; index < scAddressArr.length && !hasInteracted; index++) {
+    const addressElement = scAddressArr[index];
 
-    const expectedToken = hexData.split("@")[6];
-
-    if (
-      BytesValue.fromHex(expectedToken).toString() ===
-      selectedNetwork.tokensID.bsk
-    ) {
-      hasInteracted = true;
+    if (excludeSmartContracts.includes(addressElement[0])) {
+      continue;
     }
-  });
+
+    for (let j = 0; j < results.length && !hasInteracted; j++) {
+      const tx = results[j];
+
+      if (
+        tx.receiver === addressElement[1] ||
+        tx.action?.arguments?.receiver === addressElement[1]
+      ) {
+        hasInteracted = true;
+      }
+    }
+  }
 
   return hasInteracted;
 };
