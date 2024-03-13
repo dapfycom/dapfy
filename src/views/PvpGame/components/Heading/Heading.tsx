9d0c9b584971d3f74d88fda8b78f0c04950730a6
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-03-13 17:24:04 -0400

    Refactor Heading component and add winning/losing odds buttons

--- src/views/PvpGame/components/Heading/Heading.tsx
+++ src/views/PvpGame/components/Heading/Heading.tsx
@@ -15,19 +15,8 @@ const Heading = () => {
         <span className={"gradienteTitle"}>Player Vs Player</span>
       </PageHeaderHeading>
 
-      <div key="1" className="flex items-center justify-center w-full">
-        <div className="flex w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 overflow-hidden shadow-lg transform transition-transform hover:scale-100">
-          <button className="flex w-1/2 items-center justify-center bg-[#34D399] py-2 text-sm font-bold text-white hover:bg-[#22A884] transition-colors rounded-l-lg">
-            Winning odds 50%
-          </button>
-          <button className="flex w-1/2 items-center justify-center bg-red-500 py-2 text-sm font-bold text-white hover:bg-red-700 transition-colors rounded-r-lg">
-            Loosing odds 50%
-          </button>
-        </div>
-      </div>
-
       {price && isLoggedIn && (
-        <p className="text-4xl font-bold mt-5">
+        <p className="text-4xl font-bold mb-5">
           Total Won:{" "}
           {formatBalanceDollar(
             {
@@ -39,6 +28,17 @@ const Heading = () => {
           $
         </p>
       )}
+
+      <div key="1" className="flex items-center justify-center w-full">
+        <div className="flex w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 overflow-hidden shadow-lg transform transition-transform hover:scale-100">
+          <button className="flex w-1/2 items-center justify-center bg-[#34D399] py-2 text-sm font-bold text-white hover:bg-[#22A884] transition-colors rounded-l-lg">
+            Winning odds 50%
+          </button>
+          <button className="flex w-1/2 items-center justify-center bg-red-500 py-2 text-sm font-bold text-white hover:bg-red-700 transition-colors rounded-r-lg">
+            Loosing odds 50%
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
