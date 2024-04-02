ab3533aac2a38672be1b4b3b62861fa382cbb12c
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-04-02 12:09:04 -0400

    Add rewardsTokens to Rewards component

--- src/views/RewardsView/Rewards.tsx
+++ src/views/RewardsView/Rewards.tsx
@@ -7,11 +7,15 @@ import LoginButton from "./common/LogginButton";
 import NFTsStaking from "./common/NFTsStaking/NFTsStaking";
 import Participants from "./common/Participants";
 import StreakDays from "./common/StreakeDays";
+import { useGetUserInfo } from "./lib/nfts-hooks";
 import { useBindXUserWithDapfyUser, useStreakDialog } from "./lib/tasks-hooks";
 
 const Rewards = () => {
   useBindXUserWithDapfyUser();
   useStreakDialog();
+
+  const { rewardsTokens } = useGetUserInfo();
+
   return (
     <Container className="mt-10 flex flex-col gap-10 max-w-[800px] text-center justify-center">
       <DashboardHeading />
@@ -25,8 +29,7 @@ const Rewards = () => {
       <StreakDays />
 
       <CollectedEgld />
-
-      <NFTsStaking />
+      {rewardsTokens.length > 0 && <NFTsStaking />}
 
       {/* <EmailReports /> */}
     </Container>
