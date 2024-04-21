2f5c7a1a8728fbecf8b9fd0f312f48d915e25af7
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-04-21 15:53:39 -0400

    update gas

--- src/views/DustView/lib/services.ts
+++ src/views/DustView/lib/services.ts
@@ -94,7 +94,7 @@ export const convertTokens = async (
         new TokenIdentifierValue(tokenToReceive),
         new BigUIntValue(new BigNumber(minAmountToReceive)),
       ],
-      gasL: tokensToSend.length * 20000000,
+      gasL: tokensToSend.length * 30000000,
     }
   );
 
