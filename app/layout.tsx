a5f0bdc1b78653db03771833c96baaa38310922a
Armando C Martin <armandoc9943@gmail.com>
2024-09-17 12:30:20 -0400

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,14 +24,21 @@ export const metadata: Metadata = {
     title: "Dapfy - Monetise Your Creativity",
     description:
       "Join the waitlist for Dapfy, the ultimate platform for creators to sell digital products.",
-    images: [{ url: "/og-image.jpg", width: 1200, height: 630, alt: "Dapfy" }],
+    images: [
+      {
+        url: "/android-chrome-384x384.png",
+        width: 1200,
+        height: 630,
+        alt: "Dapfy",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Dapfy - Monetise Your Creativity",
     description:
       "Join the waitlist for Dapfy, the ultimate platform for creators to sell digital products.",
-    images: ["/twitter-image.jpg"],
+    images: ["/android-chrome-384x384.png"],
   },
 };
 
@@ -48,6 +55,27 @@ export default function RootLayout({
           data-domain="dapfy.com"
           src="https://plausible.io/js/script.js"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
